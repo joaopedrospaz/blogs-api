bbca8b31825b330f3dd946f8af4f709e4565ec52
joao <joaopaz79@outlook.com>
2023-03-06 14:43:33 -0300

    Finaliza requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+const CategoryModel = (sequelize, DataTypes) => {
+    const category = sequelize.define('Category', {
+        id: {
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+        },
+        name: DataTypes.STRING
+    },
+    {
+        tableName: 'categories',
+        timestamps: false,
+        underscored: true,
+    })
+    return category;
+}
+
+module.exports = CategoryModel;
